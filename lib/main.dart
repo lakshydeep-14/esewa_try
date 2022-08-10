7f86e62e5aa6db6a6b3a972a61c567fc6acbe5e7
Lakshy-1 <lvikramsah@gmail.com>
2022-08-10 22:24:35 +0545

    esewa implemented

---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:esewa_pnp/esewa.dart';
+import 'package:flutter/material.dart';
+
+import 'package:esewa_pnp/esewa_pnp.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ESewaPnp? _esewaPnp;
+  final ESewaConfiguration _configuration = ESewaConfiguration(
+    clientID: "JB0BBQ4aD0UqIThFJwAKBgAXEUkEGQUBBAwdOgABHD4DChwUAB0R",
+    secretKey: "BhwIWQQADhIYSxILExMcAgFXFhcOBwAKBgAXEQ==",
+    environment: ESewaConfiguration.ENVIRONMENT_TEST,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _esewaPnp = ESewaPnp(configuration: _configuration);
+  }
+
+  double _amount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(65, 161, 36, 1),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ESewa PNP"),
+          elevation: 0,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _amount = double.parse(value);
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: "Enter amount",
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "Default",
+                style: TextStyle(color: Colors.black45),
+              ),
+              ESewaPaymentButton(
+                _esewaPnp!,
+                amount: _amount,
+                callBackURL: "https://example.com",
+                productId: "abc123",
+                productName: "Flutter SDK Example",
+                onSuccess: (result) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(result.message.toString())));
+                },
+                onFailure: (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(e.message.toString())));
+                },
+              ),
+              SizedBox(
+                height: 84,
+              ),
+              Text(
+                "With White or Custom color background",
+                style: TextStyle(color: Colors.black45),
+              ),
+              ESewaPaymentButton(
+                _esewaPnp!,
+                amount: _amount,
+                callBackURL: "https://example.com",
+                productId: "abc123",
+                productName: "Flutter SDK Example",
+                onSuccess: (result) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(result.message.toString())));
+                },
+                onFailure: (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(e.message.toString())));
+                },
+                color: Colors.red,
+              ),
+              SizedBox(
+                height: 84,
+              ),
+              Text(
+                "Label Builder",
+                style: TextStyle(color: Colors.black45),
+              ),
+              ESewaPaymentButton(_esewaPnp!,
+                  amount: _amount,
+                  callBackURL: "https://example.com",
+                  productId: "abc123",
+                  productName: "Flutter SDK Example", onSuccess: (result) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(result.message.toString())));
+              }, onFailure: (e) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(e.message.toString())));
+              }, labelBuilder: (double? amount, Widget? esewaLogo) {
+                return Text("Pay Rs.$amount");
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
